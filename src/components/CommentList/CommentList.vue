1870195609dd247ee64fbfa1796e95cf5df93bd0
Horizon-srt <horizon_srt@outlook.com>
2022-11-02 19:20:56 +0800

    前端题解评论展示逻辑调整

--- src/components/CommentList/CommentList.vue
+++ src/components/CommentList/CommentList.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
-    <a-comment v-for="item in comments" :key="item.id">
+    <a-comment v-for="item in currentComments" :key="item.id">
       <Reply :solutionId="item.solutionId" :fatherNode="item.id" label="回复" />
       <a slot="author">{{ item.speaker }}</a>
       <p slot="content">
         {{ item.description }}
       </p>
-      <CommentList :comments="item.kids" />
+      <CommentList :isRoot="false" :solutionID="0" :comments="item.kids" />
     </a-comment>
   </div>
 </template>
 
 <script>
+import { getSolutionComments } from '@/api/solution_comments'
+import { boolean, number } from 'yargs'
 import Reply from './Reply'
 
 export default {
@@ -20,17 +22,72 @@ export default {
   },
   name: 'CommentList',
   props: {
+    isRoot: {
+      type: boolean,
+      required: true
+    },
+    solutionID: {
+      type: number,
+      required: true
+    },
     comments: {
       type: Array,
       required: true
     }
   },
   data () {
-    return {}
+    return {
+      currentComments: []
+    }
   },
-  methods: {},
+  methods: {
+    fatch () {
+      if (this.isRoot) {
+        // this.currentComments = [
+        // {
+        //   id: 1,
+        //   speaker: '陌溪',
+        //   description: '我是一级评论',
+        //   solutionId: 1,
+        //   fatherNode: 0,
+        //   kids: [
+        //     {
+        //       id: 2,
+        //       speaker: '陌溪',
+        //       description: '我是二级评论',
+        //       solutionId: 1,
+        //       fatherNode: 1,
+        //       kids: [
+        //         {
+        //           id: 3,
+        //           speaker: '陌溪',
+        //           description: '我是三级评论',
+        //           solutionId: 1,
+        //           fatherNode: 2,
+        //           kids: []
+        //         }
+        //       ]
+        //     }, {
+        //       id: 4,
+        //       speaker: '陌溪',
+        //       description: '我是二级评论',
+        //       solutionId: 1,
+        //       fatherNode: 1,
+        //       kids: []
+        //     }
+        //   ]
+        // }
+        // ]
+        getSolutionComments(this.solutionID).then(data => {
+
+        })
+      } else {
+        this.currentComments = this.comments
+      }
+    }
+  },
   mounted () {
-    // this.fatch()
+    this.fatch()
     // this.render()
   }
 }
--- src/views/solution/Solutions.vue
+++ src/views/solution/Solutions.vue
@@ -1,6 +1,5 @@
 <template>
   <a-list item-layout="vertical" size="large" :data-source="solutions">
-    <!-- <a-spin :spinning="solution_loading" class="solution_spin"> -->
     <a-list-item slot="renderItem" slot-scope="item">
       <a-row :gutter="[16,16]" style="height: 100%">
         <a-col :xl="{span:14, offset:2}" :lg="{span:16}" style="height: 100%">
@@ -15,13 +14,12 @@
           <a-card
             title="高人指点"
             :column="3">
-            <CommentList :comments="comments" />
+            <CommentList :isRoot="true" :solutionID="item.id" :comments="[]" />
             <Reply :solutionId="item.id" :fatherNode="0" label="我就是高人" />
           </a-card>
         </a-col>
       </a-row>
     </a-list-item>
-    <!-- </a-spin> -->
   </a-list>
 </template>
 
@@ -41,43 +39,44 @@ export default {
   data () {
     return {
       problemID: this.$route.params.id,
-      problem_loading: false,
-      solutions: [],
-      comments: [
-        {
-          id: 1,
-          speaker: '陌溪',
-          description: '我是一级评论',
-          solutionId: 1,
-          fatherNode: 0,
-          kids: [
-            {
-              id: 2,
-              speaker: '陌溪',
-              description: '我是二级评论',
-              solutionId: 1,
-              fatherNode: 1,
-              kids: [
-                {
-                  id: 3,
-                  speaker: '陌溪',
-                  description: '我是三级评论',
-                  solutionId: 1,
-                  fatherNode: 2,
-                  kids: []
-                }
-              ]
-            }, {
-              id: 4,
-              speaker: '陌溪',
-              description: '我是二级评论',
-              solutionId: 1,
-              fatherNode: 1,
-              kids: []
-            }
-          ]
-        }
-      ]
+      solution_loading: false,
+      solutions: []
+      // comments: []
+      // comments: [
+      //   {
+      //     id: 1,
+      //     speaker: '陌溪',
+      //     description: '我是一级评论',
+      //     solutionId: 1,
+      //     fatherNode: 0,
+      //     kids: [
+      //       {
+      //         id: 2,
+      //         speaker: '陌溪',
+      //         description: '我是二级评论',
+      //         solutionId: 1,
+      //         fatherNode: 1,
+      //         kids: [
+      //           {
+      //             id: 3,
+      //             speaker: '陌溪',
+      //             description: '我是三级评论',
+      //             solutionId: 1,
+      //             fatherNode: 2,
+      //             kids: []
+      //           }
+      //         ]
+      //       }, {
+      //         id: 4,
+      //         speaker: '陌溪',
+      //         description: '我是二级评论',
+      //         solutionId: 1,
+      //         fatherNode: 1,
+      //         kids: []
+      //       }
+      //     ]
+      //   }
+      // ]
       // id: this.$route.params.id,
       // problem_loading: false
     }
@@ -89,7 +88,7 @@ export default {
     fetch () {
       // this.solution_loading = true
       getSolutions(this.problemID).then(data => {
-        this.solution_loading = false
+        // this.solution_loading = false
         this.solutions = data.solutions
       }).catch(err => {
         this.$error({
